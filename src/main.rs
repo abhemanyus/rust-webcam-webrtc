7255cbb47253643e3e39ca698b5a35d64778e724
Abhemanyu Sarin <abhemanyus@gmail.com>
2022-10-06 12:29:42 +0530

    msg

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ use tas_rust::{setup, Config};
 #[tokio::main]
 async fn main() -> Result<()> {
     let config = if let Some(path) = args().nth(1) {
+        println!("Loading config at {}", &path);
         Config::parse(&path)?
     } else {
         println!("no config found, using default");
